9934c0e915232799b8421e7430eca3de7ff74b66
Aiko Zhao <57778547+aikozhaoz@users.noreply.github.com>
2021-08-30 23:57:59 -0400

    Add files via upload

---
+++ CoronaStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaStats
+//
+//  Created by Aiko Zhao on 10/27/20.
+//  Copyright © 2020 Aiko Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
